8d17abd3843773714dc4c8d8c4cd800cc86e6950
aymi27 <166910421+aymi27@users.noreply.github.com>
2024-04-18 16:05:50 +0800

    Update teach.js

--- scripts/cmds/teach.js
+++ scripts/cmds/teach.js
@@ -1,34 +1,24 @@
-const axios = require("axios");
-
-module.exports = {
-	config: {
-		name: "teach",
-		aliases: ["simteach"],
-		version: "1.0",
-		author: "KENLIEPLAYS",
-		countDown: 5,
-		role: 0,
-		shortDescription: {
-			en: "teach sammy"
-		},
-		longDescription: {
-			en: "teach sammy"
-		},
-		category: "teach",
-		guide:{
-			en: "{p}teach your ask | my answer "
-		}
-	},
-	onStart: async function ({ api, event, args }) {
-		const { messageID, threadID, senderID, body } = event;
-		const tid = threadID,
-					mid = messageID;
-		const content = args.join(" ").split("|").map(item => item.trim());
-		const ask = encodeURIComponent(content[0]);
-		const ans = encodeURIComponent(content[1]);
-		if (!args[0]) return api.sendMessage("Use .teach your ask | Sammy respond", tid, mid);
-		const res = await axios.get(`https://simsimi.fun/api/v2/?mode=teach&lang=en&message=${ask}&answer=${ans}`);
-		const responseMessage = res.data.success;
-		api.sendMessage(responseMessage, tid, mid);
-	}
+const name = global.config.BOTNAME;
+const bot = name.toLowerCase();
+module.exports.config = {
+  name: "teach", 
+  version: "1.0.0",
+  hasPermission: 0,
+  credits: "Prince Sanel Osorio",
+  description: `Teach ${name} Bot`,
+  commandCategory: "Random",
+  usages: `${global.config.PREFIX}teach message => respond`,
+  cooldowns: 0,
 };
+module.exports.run = async function ({ api, event, args }) {
+	var { threadID, messageID } = event;
+	const axios = require("axios");
+	try {
+		const request = args.join(" ").split(" => ");
+		if (!request[0] && !request[1]) return api.sendMessage(`[!] Provide an message to teach.\n\n> ${global.config.PREFIX}${this.config.name} pogi => ako`, threadID, messageID);
+		const res = await axios.get(`https://mainapi.princemc166.repl.co/api/teach?message=${encodeURI(request[0])}&respond=${encodeURI(request[1])}`);
+		api.sendMessage(res.data.message, threadID, messageID);
+	} catch (error) {
+		api.sendMessage("An error occured while fetching the sim api", threadID, messageID);
+	}
+}
