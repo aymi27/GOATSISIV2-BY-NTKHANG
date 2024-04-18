2ba02f5e6a387281dca4483ebf729eacdacac1c7
aymi27 <166910421+aymi27@users.noreply.github.com>
2024-04-18 16:05:00 +0800

    Update sim.js

--- scripts/cmds/sim.js
+++ scripts/cmds/sim.js
@@ -1,87 +1,25 @@
-const fs = require('fs');
-const path = require('path');
-
-const jsonFilePath = path.join(__dirname, 'response.json');
-
-let simResponses = {};
-
+const name = global.config.BOTNAME;
+const bot = name.toLowerCase();
 module.exports.config = {
-  name: 'sisim',
-  version: '1.0.0',
-  hasPermssion: 0,
-  credits: 'Rickciel',
-  usePrefix: false,
-  description: 'Talk with simsimi',
-  commandCategory: 'Fun',
-  usages: '[sim <input] [teach <input> - <response> / <input>]',
-  cooldowns: 3
+  name: `${bot}`, 
+  version: "1.0.0",
+  hasPermission: 0,
+  credits: "Prince Sanel Osorio",
+  description: `Chat with ${name} Bot`,
+  commandCategory: "Random",
+  usages: `${global.config.PREFIX}${bot} message`,
+  cooldowns: 0,
 };
 
-module.exports.run = ({ api, event, args }) => {
-  const arg = args.join(' ').trim();
-
-  if (!arg) {
-    return api.sendMessage('Hi there', event.threadID);
-  }
-
-  if (arg.startsWith('teach ')) {
-    const teachArgs = arg.slice(6).split(' - ');
-
-    if (teachArgs.length === 2) {
-      const input = teachArgs[0].trim().toLowerCase();
-      const response = teachArgs[1].trim();
-
-      if (!simResponses[input]) {
-        simResponses[input] = [];
-      }
-
-      if (Array.isArray(simResponses[input])) {
-        simResponses[input].push(response);
-        saveSimResponses();
-
-        const count = simResponses[input].length;
-        const times = count === 1 ? 'time' : 'times';
-        return api.sendMessage(`i learned how to respond to "${input}" with "${response}" for ${count} ${times}.`, event.threadID);
-      } else {
-        return api.sendMessage('An error occurred while teaching Sim.', event.threadID);
-      }
-    }
-  } else {
-    const responseArray = simResponses[arg.toLowerCase()];
-    if (responseArray && responseArray.length > 0) {
-      const randomIndex = Math.floor(Math.random() * responseArray.length);
-      const response = responseArray[randomIndex];
-      return api.sendMessage(response, event.threadID);
-    }
-  }
-
-  return api.sendMessage('i dont know know how to respond to that please teach me.', event.threadID);
-};
-
-function saveSimResponses() {
-  const data = {
-    responses: simResponses
-  };
-
-  fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 2), 'utf-8');
+module.exports.run = async function ({ api, event, args }) {
+	var { threadID, messageID } = event;
+	const axios = require("axios");
+	try {
+		const request = args.join(" ");
+		if (!request) return api.sendMessage("[!] Need an message to proceed!", threadID, messageID);
+		const res = await axios.get(`https://mainapi.princemc166.repl.co/api/sim?message=${request}`);
+		api.sendMessage(res.data.message, threadID, messageID);
+	} catch (error) {
+		api.sendMessage("An error occured while fetching the sim api", threadID, messageID);
+	}
 }
-
-function loadSimResponses() {
-  try {
-    const rawData = fs.readFileSync(jsonFilePath, 'utf-8');
-    const data = JSON.parse(rawData);
-    simResponses = data.responses || {};
-
-  
-    for (const input in simResponses) {
-      if (typeof simResponses[input] === 'string') {
-        simResponses[input] = [simResponses[input]];
-      }
-    }
-  } catch (error) {
-    
-  }
-}
-
-
-loadSimResponses();
